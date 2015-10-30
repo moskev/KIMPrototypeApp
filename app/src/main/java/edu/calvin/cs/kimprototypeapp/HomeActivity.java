d3a00ad98621f577cb2329efc171068cad201f63
moskev <moskev_393@yahoo.com>
2015-10-30 02:16:42 -0400

    Added a stock

--- app/src/main/java/edu/calvin/cs/kimprototypeapp/HomeActivity.java
+++ app/src/main/java/edu/calvin/cs/kimprototypeapp/HomeActivity.java
@@ -76,7 +76,7 @@ public static class PlaceholderFragment extends Fragment {
 
         // Create some dummy data for the ListView.  Here's a sample weekly forecast
         String[] data = {
-                "CHE - Servies Sector", "THOR", "CNS", "LBY", "EXLS", "ANIK", "MOV", "MTX", "CJES", "KWR", "ALG", "EGBN", "EFII", "BEKA'S STOCK!!!", "Andrew's stock"
+                "CHE - Servies Sector", "THOR", "CNS", "LBY", "EXLS", "ANIK", "MOV", "MTX", "CJES", "KWR", "ALG", "EGBN", "EFII", "BEKA'S STOCK!!!", "Andrew's stock", "Moses' Super Stock"
         };
 
         //takes array of data and stores it as a list
