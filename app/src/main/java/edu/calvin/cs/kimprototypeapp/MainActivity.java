2551993a73185306aeae24d24ca3d59562b84ce0
agroenewold <groenewold.andrew@gmail.com>
2015-11-23 23:51:31 -0500

    Talks to server now.

--- app/src/main/java/edu/calvin/cs/kimprototypeapp/MainActivity.java
+++ app/src/main/java/edu/calvin/cs/kimprototypeapp/MainActivity.java
@@ -2,12 +2,25 @@ package edu.calvin.cs.kimprototypeapp;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.protocol.BasicHttpContext;
+import org.apache.http.protocol.HttpContext;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 /***
  * MainActivity is the first activity to appear on the screen.  It prompts the user for a username and password.
@@ -26,18 +39,91 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         loginButton=(Button) findViewById(R.id.loginbutton); //asign login button
-       kimLogo = (ImageView) findViewById(R.id.kimLogo);
+        kimLogo = (ImageView) findViewById(R.id.kimLogo);
         kimLogo.setImageResource(R.mipmap.knight_investment_management);
 
         //create onClick listener for login button bringing to HomeActivity
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                loginButton.setClickable(false);
+                new LongRunningGetIO().execute();
                 //create and start new Intent
                 Intent home = new Intent(MainActivity.this, PortfolioActivity.class);
                 startActivity(home);
             }
         });
+    }
+
+    private class LongRunningGetIO extends AsyncTask<Void, Void, String> {
+
+        /**
+         * This method extracts text from the HTTP response entity.
+         *
+         * @param entity thing
+         * @return string
+         * @throws IllegalStateException
+         * @throws IOException
+         */
+        String getASCIIContentFromEntity(HttpEntity entity) throws IllegalStateException, IOException {
+            InputStream in = entity.getContent();
+            StringBuilder out = new StringBuilder();
+            int n = 1;
+            while (n > 0) {
+                byte[] b = new byte[4096];
+                n = in.read(b);
+                if (n > 0) out.append(new String(b, 0, n));
+            }
+            return out.toString();
+        }
+
+        /**
+         * This method issues the HTTP GET request.
+         *
+         * @param params and stuff
+         * @return text
+         */
+        @Override
+        protected String doInBackground(Void... params) {
+            HttpClient httpClient = new DefaultHttpClient();
+            HttpContext localContext = new BasicHttpContext();
+            /*
+      This inner class sends an HTTP requests to the Monopoly RESTful service API. It uses an
+      asynchronous task to take the slow I/O off the main interface thread.
+      <p/>
+      It uses 10.0.2.2 to access localhost, see
+      http://developer.android.com/tools/devices/emulator.html#networkaddresses
+      <p/>
+      It retains the deprecated classes in order to remain backwards compatible for Android 4, see
+      http://stackoverflow.com/questions/29150184/httpentity-is-deprecated-on-android-now-whats-the-alternative
+     */
+            String PEOPLE_URI = "http://10.0.2.2:9998/kimSQL/accounts";
+            HttpGet httpGet = new HttpGet(PEOPLE_URI);
+            String text;
+            try {
+                HttpResponse response = httpClient.execute(httpGet, localContext);
+                HttpEntity entity = response.getEntity();
+                text = getASCIIContentFromEntity(entity);
+            } catch (Exception e) {
+                return e.getLocalizedMessage();
+            }
+            return text;
+        }
+
+        /**
+         * The method takes the results of the request, when they arrive, and updates the interface.
+         *
+         * @param results
+         */
+        protected void onPostExecute(String results) {
+            /*if (results != null) {
+                EditText et = (EditText) findViewById(R.id.my_edit);
+                et.setText(results);
+            }*/
+            //Button b = (Button) findViewById(R.id.my_button);
+            loginButton.setClickable(true);
+        }
+
     }
 
     @Override
