d9aec9017bf6108e78602d193851bf59c6b12b2a
agroenewold <groenewold.andrew@gmail.com>
2015-11-25 02:16:27 -0500

    Several communications with the sever were added. So was documentaion. Some pair programming with Beka.

--- app/src/main/java/edu/calvin/cs/kimprototypeapp/DetailActivity.java
+++ app/src/main/java/edu/calvin/cs/kimprototypeapp/DetailActivity.java
@@ -15,6 +15,16 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.protocol.BasicHttpContext;
+import org.apache.http.protocol.HttpContext;
+
+import java.io.IOException;
+import java.io.InputStream;
 import java.math.BigDecimal;
 
 import yahoofinance.Stock;
@@ -26,6 +36,7 @@ It will be accessible by clicking on a specific stock's name elsewhere in the pr
  */
 
 public class DetailActivity extends Activity {
+    private TextView currentPriceField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,13 +52,13 @@ public class DetailActivity extends Activity {
         Intent intent = getIntent();
         String stockName = intent.getStringExtra(Intent.EXTRA_TEXT);
 
-        //when receive info from database would set arrow accordin to code from database
+        //when we receive info from the database, this will set the arrow according to code from the database
         ImageView arrowView = (ImageView) findViewById(R.id.arrowImage);
 
-       //make sure get stock name correctly
+       //makes sure get stock name correctly
         Log.i("STOCK Name:", stockName);
 
-        //does stock contain correct variable to make arrow green?
+        //Checks if the stock contains the correct variable to make the arrow green
         boolean value = stockName.contains("BEKA");
         String truthValue;
         if (value == true){
@@ -59,18 +70,20 @@ public class DetailActivity extends Activity {
         Log.i("TRUE/FALSE", truthValue);
 
 
-        //if stock contains correct variable to make arrow green, make it green
+        //if the stock contains the correct variable to make the arrow green, it makes it green
         if (value) {
             arrowView.setImageResource(R.mipmap.up_arrow);
         }
-        //otherwise, make it red
+        //otherwise, it makes it red
         else {
             arrowView.setImageResource(R.mipmap.down_arrow);
         }
+        //Sets up a textbox and makes an AsyncTask to fill it with the stock name
+        //This is part of our efforts to use the YahooFinance API
         TextView stockText = (TextView) findViewById(R.id.stockText);
         MyTask myTask1 = new MyTask(stockText);
         myTask1.execute(stockName);
-        //needed a change to push
+        //These next comment blocks are more attempts to use the YahooFinance API
         /*try {
             Stock stock = YahooFinance.get(stockName);
 
@@ -90,8 +103,12 @@ public class DetailActivity extends Activity {
             BigDecimal price = stock.getQuote(true).getPrice();
             System.out.println(price);
         } catch (Exception e) {}*/
+        //Creates a text box and runs an Async task to fill it with the current price of a stock
+        currentPriceField=(TextView) findViewById(R.id.currentPriceField);
+        new LongRunningGetIO().execute();
     }
 
+    //Beginning of the class that will get stock information via Yahoo Finance API
     public class MyTask extends AsyncTask<String, Integer, String> {
         private TextView myTextview;
         public MyTask(final TextView textView){
@@ -121,9 +138,78 @@ public class DetailActivity extends Activity {
 
         @Override
         protected void onPostExecute(String result){
+            //Sets the stock name to the textbox
             myTextview.setText(result);
             super.onPostExecute(result);
         }
+    }
+
+    //Class to run a database query to get a stock's current price
+    private class LongRunningGetIO extends AsyncTask<Void, Void, String> {
+
+        /**
+         * This method extracts text from the HTTP response entity.
+         *
+         * @return string, the current price
+         * @throws IllegalStateException
+         * @throws IOException
+         */
+        String getASCIIContentFromEntity(HttpEntity entity) throws IllegalStateException, IOException {
+            InputStream in = entity.getContent();
+            StringBuilder out = new StringBuilder();
+            int n = 1;
+            while (n > 0) {
+                byte[] b = new byte[4096];
+                n = in.read(b);
+                if (n > 0) out.append(new String(b, 0, n));
+            }
+            return out.toString();
+        }
+
+        /**
+         * This method issues the HTTP GET request.
+         *
+         * @param params and stuff
+         * @return text, the price
+         */
+        @Override
+        protected String doInBackground(Void... params) {
+            HttpClient httpClient = new DefaultHttpClient();
+            HttpContext localContext = new BasicHttpContext();
+            /*
+      This inner class sends an HTTP requests to the Monopoly RESTful service API. It uses an
+      asynchronous task to take the slow I/O off the main interface thread.
+      <p/>
+      It uses 10.0.2.2 to access localhost, see
+      http://developer.android.com/tools/devices/emulator.html#networkaddresses
+      <p/>
+      It retains the deprecated classes in order to remain backwards compatible for Android 4, see
+      http://stackoverflow.com/questions/29150184/httpentity-is-deprecated-on-android-now-whats-the-alternative
+     */
+            //I was having string comparison problems so currently this only returns the price of the stock with 270 shares owned
+            String PEOPLE_URI = "http://10.0.2.2:9998/kimSQL/stock/270";
+            HttpGet httpGet = new HttpGet(PEOPLE_URI);
+            String text;
+            try {
+                HttpResponse response = httpClient.execute(httpGet, localContext);
+                HttpEntity entity = response.getEntity();
+                text = getASCIIContentFromEntity(entity);
+            } catch (Exception e) {
+                return e.getLocalizedMessage();
+            }
+            return text;
+        }
+
+        /**
+         * The method takes the results of the request, when they arrive, and updates the interface.
+         *
+         * @param results, the price
+         */
+        protected void onPostExecute(String results) {
+            //Prints the label and price in the textbox
+            currentPriceField.setText("Current Price: " + results);
+        }
+
     }
 
     @Override
