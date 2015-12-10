9b83decfbbcac35cec84954d0d2279d952985a33
lac26 <lac26@students.calvin.edu>
2015-12-10 14:23:30 -0500

    Data to Fragment & Parsing String

--- app/src/main/java/edu/calvin/cs/kimprototypeapp/HomeActivity.java
+++ app/src/main/java/edu/calvin/cs/kimprototypeapp/HomeActivity.java
@@ -37,11 +37,18 @@ public class HomeActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        Bundle bundle = new Bundle();
+        bundle.putString("STOCKS", dbStocks);
+
+        PlaceholderFragment fragment = new PlaceholderFragment();
+        fragment.setArguments(bundle);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         if (savedInstanceState == null) {
             getFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment()) //starts placeholder fragment
+                    .add(R.id.container, fragment) //starts placeholder fragment
                     .commit();
         }
         //If SERVER running would execute this:
@@ -173,13 +180,28 @@ public static class PlaceholderFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
+        String stockString="";
+
+
+
+       ///get string from server passed to Placeholder Fragment as an argument
+        Bundle bundle = this.getArguments();
+        if (bundle != null) {
+            stockString = bundle.getString("STRING", "AAPL\n" + "CSCO\n" + "EA\n" + "FB\n" + "GOOG\n"); //the 2nd param is the default, i.e. if it cannot
+            //read in from the database
+        }
+
+
+        //parse the string into an array of string
+        String[] stockData = stockString.split("\\n");
+
         // Create dummy stock data for ListView
-        String[] data = {
-                "AAPL", "CSCO", "EA", "FB", "GOOG", "IBM", "JPM", "MSFT", "ORCL", "PM", "SBUX", "TSLA", "UA", "TLU", "XOM", "ZNGA", "BEKA'S STOCK!!!", "Chicken Stock"
-        };
+       // String[] data = {
+        //        "AAPL", "CSCO", "EA", "FB", "GOOG", "IBM", "JPM", "MSFT", "ORCL", "PM", "SBUX", "TSLA", "UA", "TLU", "XOM", "ZNGA", "BEKA'S STOCK!!!", "Chicken Stock"
+//        };
 
         //takes array of data and stores it as a list
-        List<String> stocks = new ArrayList<String>(Arrays.asList(data));
+        List<String> stocks = new ArrayList<String>(Arrays.asList(stockData));
 
         //Adapter takes data from the source and uses it to populate ListView
         mStockAdapter = new ArrayAdapter<String>(
@@ -202,7 +224,6 @@ public static class PlaceholderFragment extends Fragment {
                 String stockName = mStockAdapter.getItem(position);
                 //Toast.makeText(getActivity(), forecast, Toast.LENGTH_SHORT).show();
                 Intent intent = new Intent(getActivity(), DetailActivity.class).putExtra(Intent.EXTRA_TEXT, stockName);
-               // intent.putExtra(Intent.EXTRA_TEXT, stockIsUp);
                 startActivity(intent);
 
 
